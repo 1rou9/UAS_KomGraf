b3c748d4a82939763fa49b277124cead040e682f
Dimas Adi Saputro <102818370+1rou9@users.noreply.github.com>
2025-07-03 23:33:10 +0700

    histogram_volume.py

--- histogram_volume.py
+++ histogram_volume.py
@@ -10,12 +10,6 @@ data = {
         "13/12/2022", "12/12/2022", "12/09/2022", "12/08/2022", "12/07/2022", "12/06/2022",
         "12/05/2022", "12/02/2022", "12/01/2022"
     ],
-    "Harga": [
-        "Rp18.262", "Rp1.826", "Rp18.158", "Rp18.231", "Rp18.042", "Rp17.953",
-        "Rp18.254", "Rp18.254", "Rp17.977", "Rp18.002", "Rp17.878", "Rp18.187",
-        "Rp18.255", "Rp17.923", "Rp18.107", "Rp18.015", "Rp1.798", "Rp17.824",
-        "Rp17.813", "Rp18.096", "Rp18.152"
-    ],
     "Volume": [
         10750000, 10599000, 11808000, 15962000, 10546000, 17577000,
         11018000, 19750000, 8609000, 12875000, 18532000, 14380000,
@@ -24,17 +18,17 @@ data = {
     ]
 }
 
-# Format data
 df = pd.DataFrame(data)
 df["Tanggal"] = pd.to_datetime(df["Tanggal"], dayfirst=True)
 
-# Judul dan grafik
-st.title("📊 Histogram Volume Penjualan per Hari")
+# Judul
+st.title("Grafik Histogram Volume Penjualan per Hari")
 
-bar_chart = alt.Chart(df).mark_bar().encode(
-    x='Tanggal:T',
-    y='Volume:Q',
+# Grafik
+bar_chart = alt.Chart(df).mark_bar(color='skyblue').encode(
+    x=alt.X('Tanggal:T', title='Tanggal'),
+    y=alt.Y('Volume:Q', title='Volume'),
     tooltip=['Tanggal', 'Volume']
-).properties(width=800, height=400)
+).properties(width=800, height=400).configure_axisX(labelAngle=45)
 
 st.altair_chart(bar_chart, use_container_width=True)
